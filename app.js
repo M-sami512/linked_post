0ce53058011ad7614fdc89716b65ce95586a4575
M-sami512 <mohamed3573579@gmail.com>
2024-12-06 21:40:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.all('/',(req,res)=>{
     res.status(404).json({massage : "not found"})
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server running...")
 })      
